70d226ee2bcb00f31f814100d430991ba78d3126
estefanoquiriconi <estefanoquiriconi@gmail.com>
2025-02-15 20:32:28 -0300

    feat: Enhance ListaDeProductos with advanced product management methods

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
+import { readFileSync } from 'fs';
+import { orderBy } from 'lodash';
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
+
   constructor(name: string) {
-    // nombre de esta lista
     this.name = name;
   }
-  add(nuevaCosa) {
+
+  add(nuevaCosa: any) {
     this.cosas.push(nuevaCosa);
   }
+
   getCosas() {
     return this.cosas;
   }
@@ -17,6 +22,7 @@ class Product {
   name: string;
   price: number;
   id: number;
+
   constructor(name: string, price: number, id: number) {
     this.name = name;
     this.price = price;
@@ -24,6 +30,39 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const data = readFileSync(__dirname + '/products.json', 'utf-8');
+    const products: Product[] = JSON.parse(data);
+    for (const product of products) {
+      this.addProduct(product);
+    }
+  }
+
+  addProduct(product: Product): void {
+    const exists = this.getProduct(product.id);
+    if (!exists) {
+      this.add(product);
+    }
+  }
+
+  getProduct(id: number): Product {
+    const product = this.cosas.find(c => c.id === id);
+    return product;
+  }
+
+  removeProduct(id: number): Product {
+    const product = this.getProduct(id);
+
+    this.cosas = this.cosas.filter(c => c.id !== id);
+
+    return product;
+  }
+
+  getSortedByPrice(order: 'asc' | 'desc' = 'asc'): Product[] {
+    return orderBy(this.cosas, ['price'], [order]);
+  }
+}
 
 export { ListaDeProductos, Product };
